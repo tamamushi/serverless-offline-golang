7056db12b8baefca331ab684fbc342b05d27ed54
vagrant <vagrant@localhost.localdomain>
2018-07-19 17:30:36 +0900

    Added process kill statement

--- src/index.js
+++ src/index.js
@@ -437,7 +437,7 @@ class Offline {
 	p.on('error', function (err) { console.error(err); process.exit(1); });
 
 	debugLog('handlerPath: ', funOptions.handlerPath);
-	debug+og('pid: ', this.p.pid);
+	debugLog('pid: ', this.p.pid);
 
         this.server.route({
           method: routeMethod,
